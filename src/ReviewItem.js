ab3417ce111052d3454e10757563a2684f7cc35d
rooserik <rooserik@gmail.com>
2016-03-02 17:04:44 +0100

    reviewlist and reviewitem

---
+++ src/ReviewItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import jQuery from 'jquery';
+
+class ReviewItem extends React.Component {
+  constructor(){
+    super();
+  }
+
+  componentDidMount() {
+    this.setState({
+      id: this.props.id,
+      areaId: this.props.areaId,
+      story: this.props.story,
+      writer: this.props.writer
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>{this.props.writer}</h2>
+        <p>{this.props.story}</p>
+      </div>
+    );
+  }
+}
