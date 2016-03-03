9b67f5d7d35cb32a407a0720c6c331b3db71c564
rooserik <rooserik@gmail.com>
2016-03-03 22:08:49 +0100

    made a create function

--- src/ReviewItem.js
+++ src/ReviewItem.js
@@ -6,20 +6,54 @@ class ReviewItem extends React.Component {
     super();
   }
 
-  componentDidMount() {
-    this.setState({
-      id: this.props.id,
-      areaId: this.props.areaId,
-      story: this.props.story,
-      writer: this.props.writer
-    })
+  createReview(event){
+    event.preventDefault();
+
+    let component = this;
+    let areaId = this.props.areaId;
+    let writer = this.refs.inputWriter.value;
+    let rating = this.refs.inputRating.value;
+    let story = this.refs.inputStory.value;
+    let newReview = {
+      id: null,
+      story: story
+      writer: writer,
+      rating: rating,
+    };
   }
+
+    jQuery.ajax(
+      {
+        type: "POST".
+        url: "https://arearate.herokuapp.com/areas/" + areaId + "/reviews",
+        data: JSON.stringify({
+          review: newReview
+        }),
+        contentType: "application/json",
+        dataType: "json"
+      }
+    );
+
+    .done(function(data) {
+      component.props.onChange();
+      component.refs.inputWriter.value = "";
+      component.refs.inputStory.value = "";
+      component.refs.inputRating.value = "";
+    })
+  // componentDidMount() {
+  //   this.setState({
+  //     id: this.props.id,
+  //     areaId: this.props.areaId,
+  //     story: this.props.story,
+  //     writer: this.props.writer
+  //   })
+  // }
 
   render(){
     return(
       <div>
-        <h2>{this.props.writer}</h2>
-        <p>{this.props.story}</p>
+        <h2>{this.props.story}</h2>
+        <p>{this.props.writer}</p>
       </div>
     );
   }
